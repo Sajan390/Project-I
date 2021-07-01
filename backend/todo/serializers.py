3177b85bc27271f730e8e2c141419a2733af6471
Sajan390 <mr.sajankasula@gmail.com>
2021-07-01 22:39:41 +0545

    First Commit

---
+++ backend/todo/serializers.py
@@ -0,0 +1,8 @@
+import rest_framework 
+import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = Todofield = ('id', 'description', 'completed')
